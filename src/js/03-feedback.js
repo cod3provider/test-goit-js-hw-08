55d200339e956fc493b5a1dbcdf752faf0e95dfa
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2022-09-06 17:06:11 +0300

    task 03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import storage from './storage.js';
+// import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
+const formRef = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+  const formInfo = storage.load(LOCALSTORAGE_KEY);
+  const formArrKeys = Object.keys(formInfo);
+
+  formArrKeys.forEach(name =>
+    formRef.elements[name].value = formInfo[name]
+  );
+}
+
+formRef.addEventListener('input', throttle(updateStorage, 500));
+formRef.addEventListener('submit', clearLocaleStorageAndFormFields);
+
+function updateStorage(evt) {
+  console.log(evt.currentTarget);
+  const feedback = {};
+  const formData = new FormData (formRef);
+  formData.forEach((value, name) => {
+    feedback[name] = value;
+  });
+
+  storage.save(LOCALSTORAGE_KEY, feedback);
+}
+
+function clearLocaleStorageAndFormFields(evt) {
+  if (evt.currentTarget.elements.message.value) {
+    evt.preventDefault();
+    localStorage.clear();
+    formRef.reset();
+  } else {
+    alert('Не все поля заполнены. За тобой выехали!');
+  }
+}
